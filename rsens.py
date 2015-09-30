0429c4b5189e1df0b6d1bd78bd880ba00cd6e864
Nick Bartley <github@nb221.com>
2015-09-30 19:04:19 +0100

    Added try statement to catch DB connection errors.

--- rsens.py
+++ rsens.py
@@ -9,22 +9,24 @@ time.sleep(10) # wait for initialisation
 dev = usb.core.find(idVendor=0x16c0, idProduct=0x5dc)
 
 while True:
-	db = MySQLdb.connect(host="RaspberryNAS.lan", user="root", passwd="raspberry", db="decibel_monitor")
-	cur = db.cursor()
-	ret = dev.ctrl_transfer(0xC0, 4, 0, 0, 200)
-	data = (ret[0] + ((ret[1] & 3) * 256)) * 0.1 + 30
-	#print "%s dB" % data
-	
 	try:
-		cur.execute("INSERT INTO tub_raw (decibel) VALUES (%s)" % data)
-		db.commit()
-		cur.execute("INSERT INTO plug_raw (decibel) VALUES (%s)" % data)
-		db.commit()
+		db = MySQLdb.connect(host="RaspberryNAS.lan", user="root", passwd="raspberry", db="decibel_monitor")
+		cur = db.cursor()
+		ret = dev.ctrl_transfer(0xC0, 4, 0, 0, 200)
+		data = (ret[0] + ((ret[1] & 3) * 256)) * 0.1 + 30
+		#print "%s dB" % data
+		
+		try:
+			cur.execute("INSERT INTO tub_raw (decibel) VALUES (%s)" % data)
+			db.commit()
+			cur.execute("INSERT INTO plug_raw (decibel) VALUES (%s)" % data)
+			db.commit()
+		except:
+			db.rollback()
+		db.close()
+		time.sleep(0.2)
 	except:
-		db.rollback()
-	db.close()
-	time.sleep(0.2)
-
+		time.sleep(10)
 	
 
 
